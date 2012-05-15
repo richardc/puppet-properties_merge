9b4cfbd692266020c7837d39114ed0a726393f68
Richard Clamp <richardc@unixbeard.net>
2012-05-15 18:33:15 +0100

    stop using template() for tests, much annoying

--- test/merge.pp
+++ test/merge.pp
@@ -1,5 +1,33 @@
-notice( properties_merge( template("properties_merge/t1/one" ) ) )
+define testcase($one = "", $two = "", $expect = "") {
+     $got = properties_merge($one, $two)
+     if $got != $expect {
+       notice( "FAILED ${name}: '${got}' != '${expect}'" )
+     }
+     else {
+       notice( "OK ${name}" )
+     }
+}
 
-notice( properties_merge( template("properties_merge/t2/one" ), template("properties_merge/t2/two" ) ) )
+testcase { "merge_with_nothing":
+   one    => "ONE_PROPERTY      'one'",
+   expect => "ONE_PROPERTY      'one'",
+}
 
-notice( properties_merge( template("properties_merge/t3/one" ), template("properties_merge/t3/two" ) ) )
+testcase { "append perl style":
+  one    => "ONE_PROPERTY      'one'",
+  two    => "SECOND_PROPERTY 'two'",
+  expect => "ONE_PROPERTY      'one'\nSECOND_PROPERTY 'two'",
+}
+
+testcase { "override perl style":
+  one    => "ONE_PROPERTY      'one'",
+  two    => "ONE_PROPERTY 'two'",
+  expect => "ONE_PROPERTY 'two'",
+}
+
+testcase { "append java style":
+  one => "foo.bar=one\n",
+  two => "foo.baz=two\n",
+  expect => "foo.bar=one\nfoo.baz=two"
+}
+
